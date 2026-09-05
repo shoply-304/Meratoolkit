754e01e5c3ca2955a80e9e5bab3905a434f45227
shoply-304 <websiteshoply@gmail.com>
2026-09-05 10:56:43 +0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -10,14 +10,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req: Request) {
+      req: (req: any) => {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res: Response) {
+      res: (res: any) => {
         return {
           statusCode: res.statusCode,
         };
